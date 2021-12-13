edd8c3f2c653414e195d69afcc9e8106dd66954d
chullman <christopherhullman@gmail.com>
2021-12-13 11:15:24 +1100

    temp

--- dom1.js
+++ dom1.js
@@ -2,4 +2,5 @@
 window.onload = function() {
     var planet = document.getElementById("greenplanet");
     planet.innerHTML = "Red Alert: hit by phaser fire!!!";
+    planet.setAttribute("class", "redtext");
 };
